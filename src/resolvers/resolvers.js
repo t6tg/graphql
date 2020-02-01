af15d3ea3f167d8dc0fc3e3e38e64866d08d8e88
thanawatgulati <thanawatgulatijames@gmail.com>
2020-02-01 21:34:25 +0700

    🔑 bcrypt password

--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,8 +1,23 @@
 import User from '../models/user';
+import bcrypt from 'bcryptjs';
 
 const Mutation = {
-  signup: (parent, args, context, info) => {
-    return User.create(args);
+  signup: async (parent, args, context, info) => {
+    // Trim and Lower case email
+    const email = args.email.trim().toLowerCase();
+    //  Check if email already exits in database
+    const currentUsers = await User.find({});
+    const isEmailExist =
+      currentUsers.findIndex(user => user.email === email) > -1;
+    if (isEmailExist) {
+      throw new Error('Email already exist.');
+    }
+    // Validate password
+    if (args.password.trim().length() < 6) {
+      throw new Error('Password must be at least 6 characters');
+    }
+    const password = await bcrypt.hash(args.password, 10);
+    return User.create(...args, email, password);
   }
 };
 const Query = {
